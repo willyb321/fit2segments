cdc2e3f8f5645d5cd2e51997848b903a2cce7317
Aurélien Grosdidier <aurelien.grosdidier@gmail.com>
2020-06-02 09:33:39 +0200

    Much improved UI

--- ui/TODO.md
+++ ui/TODO.md
@@ -1,10 +1,18 @@
-- default to last activity
-- two searchable columns on the left:
+# Functions
 
-  - activities by date
-  - segments, those in this activity on top (+different colors)
+- Make the activities/column searchable
+- Default to summary "everything": all distance, all time, etc.
+- Date / daterange selector → summary
+- Make filtering my `gps_available` optional
+- show maps, which requires either:
+  - access to FIT `track_points`, can be made available as JSON?
+  - get them from strava directly, at least for segments?
 
-- UX:
+# Styling
 
-  - click on an activity: show summary + map + segments
-  - click on a segment: show summary + map + activity
+- <https://designshack.net/articles/css/5-simple-and-practical-css-list-styles-you-can-copy-and-paste/>
+
+# Code organization
+
+- https://cli.vuejs.org/guide/
+- https://www.jesuisundev.com/comprendre-vuejs-en-5-minutes/
--- ui/index.js
+++ ui/index.js
@@ -22,6 +22,30 @@ Vue.component("segmentDefinitionItem", {
   },
 });
 
+Vue.component("segmentInActivityContextItem", {
+  props: ["segment"],
+  template:
+    '<li><a @click="renderSegmentDefinition(segment.definition)"> {{ segment.segment_name }}</a> ({{segment.definition.strava_id}}): {{ segment.duration_str }}, {{segment.rankAllTime}}/{{segment.nbAttemptAllTime}}, {{segment.rankThisYear}}/{{segment.nbAttemptThisYear}} </li> ',
+  methods: {
+    renderSegmentDefinition(segmentDefinition) {
+      this.$root.data_to_render_type = "segmentDefinition";
+      this.$root.data_to_render = segmentDefinition;
+    },
+  },
+});
+
+Vue.component("segmentInDefinitionContextItem", {
+  props: ["segment"],
+  template:
+    '<li><a @click="renderActivity(segment.activity)">{{ segment.start_date_str }}</a>: {{ segment.duration_str }}</li>',
+  methods: {
+    renderActivity(activity) {
+      this.$root.data_to_render_type = "activity";
+      this.$root.data_to_render = activity;
+    },
+  },
+});
+
 // Convert segments loaded from JSON into proper structure (e.g. time, duration)
 const convertLoadedSegment = function convertLoadedSegment(segment) {
   const newStartTime = new Date(null);
@@ -31,14 +55,23 @@ const convertLoadedSegment = function convertLoadedSegment(segment) {
   const newDuration = new Date(null);
   newDuration.setSeconds(segment.duration);
   toReturn.duration = newDuration;
+  [toReturn.definition] = segment_definitions.filter(
+    (sg) => sg.uid === toReturn.segment_uid
+  );
+  [toReturn.activity] = activities.filter(
+    (a) => a.name === toReturn.activity_name
+  );
+
   return toReturn;
 };
 
 // Create adequate string representations for rendering a segment
 const prepareSegmentRendering = function prepareSegmentRendering(segment) {
   const toReturn = segment;
-  toReturn.start_time_str = segment.start_time.toISOString().substr(0, 10);
+  toReturn.start_time_str = segment.start_time.toISOString();
+  toReturn.start_date_str = toReturn.start_time_str.substr(0, 10);
   toReturn.duration_str = segment.duration.toISOString().substr(11, 8);
+  toReturn.year = toReturn.start_time_str.substring(0, 4);
   return toReturn;
 };
 
@@ -67,6 +100,7 @@ const app = new Vue({
 
   data: {
     activities: activities
+      .filter((a) => a.gps_available)
       .sort((a, b) => Date.parse(b.start_time) - Date.parse(a.start_time))
       .map(convertLoadedActivity)
       .map(prepareActivityRendering),
@@ -79,13 +113,35 @@ const app = new Vue({
     data_to_render_type: "",
     content: "",
   },
+  mounted: function () {
+    this.$root.data_to_render_type = "activity";
+    this.$root.data_to_render = activities[activities.length - 1];
+  },
   computed: {
     context() {
       if (this.data_to_render_type === "activity") {
-        const { name } = this.data_to_render;
         return this.segments
-          .filter((segment) => segment.activity_name === name)
-          .sort((a, b) => Date.parse(a.start_time) - Date.parse(b.start_time));
+          .filter(
+            (segment) => segment.activity_name === this.data_to_render.name
+          )
+          .sort((a, b) => Date.parse(a.start_time) - Date.parse(b.start_time))
+          .map((targetsegment) => {
+            const allTime = this.segments.filter(
+              (segment) => segment.segment_uid === targetsegment.segment_uid
+            );
+            const thisYear = allTime.filter(
+              (segment) => segment.year === targetsegment.year
+            );
+
+            const toReturn = targetsegment;
+            toReturn.rankAllTime = allTime.indexOf(targetsegment);
+            toReturn.nbAttemptAllTime = allTime.length;
+            [toReturn.bestAllTime] = allTime;
+            toReturn.rankThisYear = thisYear.indexOf(targetsegment);
+            toReturn.nbAttemptThisYear = thisYear.length;
+            [toReturn.bestThisYear] = thisYear;
+            return toReturn;
+          });
       }
       if (this.data_to_render_type === "segmentDefinition") {
         const { uid } = this.data_to_render;
